3b404c42077974fd23a3ad00bdd04fbea0ef2a3b
bohdanbtw <mybuisnessbohdan@gmail.com>
2026-04-28 15:46:57 +0300

    fix: add fallback to system Gradle wrapper jar in gradlew.bat

--- AndroidCppApp/packaging/gradlew.bat
+++ AndroidCppApp/packaging/gradlew.bat
@@ -16,7 +16,19 @@ set APP_HOME=%DIRNAME%
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
 for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
-set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+set WRAPPER_JAR=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
+if not exist "%WRAPPER_JAR%" (
+    set WRAPPER_JAR=C:\Tools\Gradle\gradle-8.10.2\lib\gradle-wrapper-8.10.2.jar
+)
+
+if not exist "%WRAPPER_JAR%" (
+    echo ERROR: Gradle wrapper jar not found.^
+ Expected at either %APP_HOME%\gradle\wrapper\gradle-wrapper.jar or C:\Tools\Gradle\gradle-8.10.2\lib\gradle-wrapper-8.10.2.jar
+    exit /b 1
+)
+
+set CLASSPATH=%WRAPPER_JAR%
 
 @rem Execute Gradle
 "%JAVA_HOME%\bin\java.exe" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
