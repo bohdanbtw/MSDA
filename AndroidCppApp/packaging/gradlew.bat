827ae2098e93234ad3c5a0c78a2cfd3f03cbd997
bohdanbtw <mybuisnessbohdan@gmail.com>
2026-04-28 16:01:56 +0300

    feat: auto-detect JDK 17 in gradlew.bat if JAVA_HOME not set

--- AndroidCppApp/packaging/gradlew.bat
+++ AndroidCppApp/packaging/gradlew.bat
@@ -8,6 +8,31 @@
 @rem Set local scope with delayed expansion to allow variable changes inside blocks
 if "%OS%"=="Windows_NT" setlocal enabledelayedexpansion
 
+:: ------------------------------------------------
+:: 1. Locate a JDK 17 if JAVA_HOME is not already set
+:: ------------------------------------------------
+if "%JAVA_HOME%"=="" (
+    :: first try to find java on PATH
+    for /f "usebackq delims=" %%a in (`where java 2^>nul`) do (
+        if not defined JAVA_HOME (
+            for %%b in ("%%a") do set "JAVA_HOME=%%~dpb.."
+        )
+    )
+    :: if still empty, search common install locations
+    if not defined JAVA_HOME (
+        for %%d in (
+            "C:\Program Files\Eclipse Adoptium\jdk-17.0.12.7-hotspot"
+            "C:\Program Files\Java\jdk-17.0.11"
+            "C:\Program Files\Microsoft\jdk-17.0.12.7-hotspot"
+            "C:\Program Files\OpenJDK\jdk-17.0.11.9-hotspot"
+        ) do if exist "%%~d\bin\java.exe" (
+            if not defined JAVA_HOME (
+                set "JAVA_HOME=%%~d"
+            )
+        )
+    )
+)
+
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
@@ -19,7 +44,7 @@ for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 set WRAPPER_JAR=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
 @rem -----------------------------------------------------------------
-@rem If the wrapper jar is missing, download it automatically
+@rem 2. If the wrapper jar is missing, download it automatically
 @rem -----------------------------------------------------------------
 if not exist "%WRAPPER_JAR%" (
     echo Gradle wrapper jar not found - downloading 8.10.2 distribution ...
@@ -53,5 +78,7 @@ if not exist "%WRAPPER_JAR%" (
 
 set CLASSPATH=%WRAPPER_JAR%
 
-@rem Execute Gradle
+@rem -----------------------------------------------------------------
+@rem 3. Execute Gradle
+@rem -----------------------------------------------------------------
 "%JAVA_HOME%\bin\java.exe" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
